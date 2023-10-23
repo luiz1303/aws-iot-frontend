f5bec9d899df5d9bd210cdc01e533ec1138635b0
luiz1303 <luiz.1303@hotmail.com>
2023-10-22 21:51:09 -0300

    ✨ - feat: aws authentication with amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "iotamplifyapp31ba9e77": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminGroupRole": "string"
+    }
+  }
+}
